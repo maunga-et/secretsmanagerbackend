a4b97b92e63532ea150b52fb09c707aeb71aab62
Emmanuel Maunga <tanyahmaunga274@gmail.com>
2024-05-02 13:50:23 +0200

    add api docs on the home of api url

--- secretsmanager/urls.py
+++ secretsmanager/urls.py
@@ -18,6 +18,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("admin/", admin.site.urls),
     path("api/v1/accounts/", include("accounts.urls")),
     path("api/v1/secrets/", include("core.urls")),
